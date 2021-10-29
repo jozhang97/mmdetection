c7d15dcae3808020b6f43fdcdd8c2f6c597cfe75
JEFFREY ZHANG <jozhang@berkeley.edu>
2021-10-29 14:21:05 -0500

    add configs_uvo_det

--- tools/verify.py
+++ tools/verify.py
@@ -1,7 +1,7 @@
 from mmdet.apis import init_detector, inference_detector
 
 # config_file = 'configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
-config_file = 'configs_uvo/uvo/swin_l_carafe_simota_focal_giou_iouhead_tower_dcn_coco_384_uvo_finetune.py'
+config_file = 'configs_uvo_det/uvo/swin_l_carafe_simota_focal_giou_iouhead_tower_dcn_coco_384_uvo_finetune.py'
 
 # download the checkpoint from model zoo and put it in `checkpoints/`
 # url: https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth
